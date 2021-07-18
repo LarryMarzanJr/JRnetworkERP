3217199db8204000717d4e9143a9a2d71054eef6
Larry Marzan Jr <Larry.MarzanJr@gmail.com>
2021-07-18 19:10:26 +0800

    added product_detail_view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -31,12 +31,12 @@ def product_list_view(request):
     }
     return render(request, "products/product_list.html", context)
 
-# def product_detail_view(request, id):
-#     obj = get_object_or_404(Product, id=id)
-#     context = {
-#         "object": obj
-#     }
-#     return render(request, "products/product_detail.html", context)
+def product_detail_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    context = {
+        "object": obj
+    }
+    return render(request, "products/product_detail.html", context)
 
 
 # def product_delete_view(request, id):
