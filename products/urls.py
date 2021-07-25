1102dcae5b070439a672dc40d0f084ebd8a4c166
Larry Marzan Jr <Larry.MarzanJr@gmail.com>
2021-07-25 17:30:59 +0800

    product update url

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from .views import (
     product_detail_view, 
     # product_delete_view,
     product_list_view,
-    # product_update_view,
+    product_update_view,
     
 )
 
@@ -13,6 +13,6 @@ urlpatterns = [
     path('', product_list_view, name='product-list'),
     path('create/', product_create_view, name='product-list'),
     path('<int:id>/', product_detail_view, name='product-detail'),
-    # path('<int:id>/update/', product_update_view, name='product-update'),
+    path('<int:id>/update/', product_update_view, name='product-update'),
     # path('<int:id>/delete/', product_delete_view, name='product-delete'),
 ]
