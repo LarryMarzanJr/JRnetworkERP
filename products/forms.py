a9c94c859ad2e9b98661205e0a6991b66a120da7
Larry Marzan Jr <Larry.MarzanJr@gmail.com>
2021-07-25 17:30:21 +0800

    added featured field

--- products/forms.py
+++ products/forms.py
@@ -3,26 +3,31 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
-    title       = forms.CharField(label='', 
-                    widget=forms.TextInput(attrs={"placeholder": "Input nama produk"}))
+    title       = forms.CharField(
+                    label='', 
+                    widget=forms.TextInput(attrs={"placeholder": "Input nama produk"})
+                    )
     description = forms.CharField(
-                        required=False, 
-                        widget=forms.Textarea(
-                                attrs={
-                                    "placeholder": "Deskripsi Produk",
-                                    "class": "new-class-name two",
-                                    "id": "my-id-for-textarea",
-                                    "rows": 20,
-                                    'cols': 120
-                                }
-                            )
+                    required=False, 
+                    widget=forms.Textarea(
+                            attrs={
+                                "placeholder": "Deskripsi Produk",
+                                "class": "new-class-name two",
+                                "id": "my-id-for-textarea",
+                                "rows": 20,
+                                'cols': 120
+                            }
                         )
-    price       = forms.DecimalField(initial=200000)
+                    )
+    price       = forms.DecimalField(
+                    initial=200000
+                    )
     
     class Meta:
         model = Product
         fields = [
             'title',
             'description',
-            'price'
+            'price',
+            'featured'
         ]
